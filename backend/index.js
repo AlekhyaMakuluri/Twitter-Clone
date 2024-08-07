51ca5ebf57175cdd6b3b840ebed820875937678d
Alexa <90927047+alexaa007@users.noreply.github.com>
2024-08-08 04:12:21 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,75 @@
-const express = require('express');
-const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const uri = "mongodb+srv://admin:<password>@twitter-clone.nvhzm.mongodb.net/?retryWrites=true&w=majority&appName=twitter-clone";
+const port = 5000;
+
 const app = express()
-const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json())
 
-app.use(cors());
-app.use(express.json());
+const client = new MongoClient(uri);
 
-const uri = `mongodb+srv://alekhyamakuluri2864:vF9VN1lebCrd5t9f@cluster0.7qyqg9w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err=>{
-    const collection = client.db("test").collection("devices");
-    console.log('connected');
-    client.close();
-})
+async function run() {
+  try {
+    await client.connect();
+    const postCollection = client.db("twitter").collection("posts");
+    const userCollection = client.db("twitter").collection("users");
+
+
+    // get
+    app.get('/user', async (req, res) => {
+      const user = await userCollection.find().toArray();
+      res.send(user);
+    })
+    app.get('/loggedInUser', async (req, res) => {
+      const email = req.query.email;
+      const user = await userCollection.find({ email: email }).toArray();
+      res.send(user);
+    })
+    app.get('/post', async (req, res) => {
+      const post = (await postCollection.find().toArray()).reverse();
+      res.send(post);
+    })
+    app.get('/userPost', async (req, res) => {
+      const email = req.query.email;
+      const post = (await postCollection.find({ email: email }).toArray()).reverse();
+      res.send(post);
+    })
+
+    // post
+    app.post('/register', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.post('/post', async (req, res) => {
+      const post = req.body;
+      const result = await postCollection.insertOne(post);
+      res.send(result);
+    })
+
+    // patch
+    app.patch('/userUpdates/:email', async (req, res) => {
+      const filter = req.params;
+      const profile = req.body;
+      const options = { upsert: true };
+      const updateDoc = { $set: profile };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
+
+  } catch (error) {
+    console.log(error);
+  }
+} run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('Hello World from!')
+  res.send("twitter is working")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Twitter clone is working on ${port}`)
 })
